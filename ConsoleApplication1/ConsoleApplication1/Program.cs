2c53600c9cf1adb76700262ef23c89b3cf7840f3
hung.187pm13922 <hungluu2819@gmail.com>
2019-09-26 15:15:13 +0700

    Hung

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,12 +6,52 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
-            Console.ReadKey();
+            
         }
+
+        public static bool IsOdd(int n)
+        {
+            if (n % 2 != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsEven(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsPrime(int n)
+        {   
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void int Square (int n)
+        {
+            return n*n;
+        }
+
     }
 }
---
+++ ConsoleApplication1/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        public void IsOdd()
+        {
+            bool result3 = Program.IsOdd(3);
+            Assert.AreEqual(true, result3);
+
+            bool result4 = Program.IsOdd(4);
+            Assert.AreEqual(false, result4);
+        }
+
+          [TestMethod]
+
+        public void IsEven()
+        {
+            bool result3 = Program.IsEven(3);
+            Assert.AreEqual(false, result3);
+
+            bool result4 = Program.IsEven(4);
+            Assert.AreEqual(true, result4);
+        }
+
+        [TestMethod]
+
+          public void IsPrime()
+          {
+              bool result3 = Program.IsPrime(3);
+              Assert.AreEqual(true, result3);
+
+              bool result4 = Program.IsPrime(4);
+              Assert.AreEqual(false, result4);
+          }
+    }
+}
